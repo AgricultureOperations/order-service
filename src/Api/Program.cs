bc85f78ff55c0a70c601e19ecb7de35ba03979d6
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2026-04-07 10:04:10 -0500

    chore(auth): configure JwtBearer to validate tokens from external issuer(issuer/audience)

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,10 +1,54 @@
-    using Microsoft.EntityFrameworkCore;
-    using Application.UseCases;
-    using Domain.Ports.Driven;
-    using Infrastructure.Adapters.Driven;
-    using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Application.UseCases;
+using Domain.Ports.Driven;
+using Infrastructure.Adapters.Driven;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+
+var jwtSecret = builder.Configuration["Jwt:Secret"];
+if (string.IsNullOrEmpty(jwtSecret))
+    throw new Exception("Jwt:Secret is missing");
+var jwtValidIssuer = builder.Configuration["Jwt:Issuer"];
+if (string.IsNullOrEmpty(jwtValidIssuer))
+    throw new Exception("Jwt:Issuer is missing");
+var jwtVaidAudience = builder.Configuration["Jwt:Audience"];
+if (string.IsNullOrEmpty(jwtVaidAudience))
+    throw new Exception("Jwt:Audience is missing");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false; // true in production
+    options.SaveToken = true;
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = jwtValidIssuer,
+
+        ValidateAudience = true,
+        ValidAudience = jwtVaidAudience,
+        
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtSecret)
+        ),
+
+        RequireSignedTokens = true,
+        RequireExpirationTime = true,
+    };
+});
 
     builder.Services.AddCors(options =>
     {
@@ -39,6 +83,7 @@
         var db = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
         db.Database.Migrate();
     }
-
+    app.UseAuthentication();
+    app.UseAuthorization();
     app.MapControllers();
     app.Run();
